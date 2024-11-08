a21d42e7a735084c5086b2e8c2f35a2a1e90a0ac
GUSTAVOSCOMPARIN <141827623+GUSTAVOSCOMPARIN@users.noreply.github.com>
2024-11-08 08:38:06 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    ' Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É o processo pelo qual as plantas transformam a luz solar em energia, produzindo alimento (glicose) e liberando oxigênio.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o resultado de 6x7 ?',
+    '6x7= 42'
+)
